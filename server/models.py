0b3ea2a9c23f481b2724d70178aaf54700bf0e7f
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-09-24 16:38:24 -0600

    added validation correction

--- server/models.py
+++ server/models.py
@@ -20,7 +20,6 @@ class Restaurant(db.Model, SerializerMixin):
     name = db.Column(db.String)
     address = db.Column(db.String)
 
-    # Corrected relationship
     restaurant_pizzas = db.relationship("RestaurantPizza", back_populates="restaurant")
     
     serialize_rules = ('restaurant_pizzas.restaurant',)
