2082c72c7d7838175a196cba3183b0c98818d74a
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-09-24 16:20:24 -0600

    foreign keys

--- server/models.py
+++ server/models.py
@@ -49,6 +49,9 @@ class RestaurantPizza(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
 
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+
     # add relationships
 
     # add serialization rules
